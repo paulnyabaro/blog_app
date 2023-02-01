44f6fe07abcb8454dfcddb1b3dce83bf861ee03e
paulnyabaro <paulnyabaro1@gmail.com>
2023-02-01 07:32:21 +0300

    Setup homepage template

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import home
+from .views import BLogListView
 
 urlpatterns = [
-    path('', home.as_view(), name='home'),
+    path('', BLogListView.as_view(), name='home'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
 
-def home(request):
-    return render(request, 'home.html'),
+class BLogListView(ListView):
+    model = Post
+    template_name = 'home.html'
