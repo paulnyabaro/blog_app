3c5a712c9c828e4a410c50ff95604d39966bac74
paulnyabaro <paulnyabaro1@gmail.com>
2023-02-02 08:23:51 +0300

    Testing for correct location for url both list and sigle view

--- blog/tests.py
+++ blog/tests.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
 from .models import Post
 
@@ -22,3 +23,11 @@ class BlogTests(TestCase):
         self.assertEqual(self.post.author.username, 'testuser')
         self.assertEqual(str(self.post), 'A good title')
         self.assertEqual(self.post.get_absolute_url(), '/post/1/')
+
+    def test_url_exists_at_correct_location_listview(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_exists_at_correct_location_detailview(self):
+        response = self.client.get('/post/1/')
+        self.assertEqual(response.status_code, 200)
