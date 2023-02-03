1ba19c90dfd82039471b48b958525bd9f8c19f1f
paulnyabaro <paulnyabaro1@gmail.com>
2023-02-03 05:14:44 +0300

    Working on post edit

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
 
 
@@ -14,4 +14,9 @@ class BlogDetailView(DetailView):
 class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+class BlogEditView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
     fields = ['title', 'author', 'body']
