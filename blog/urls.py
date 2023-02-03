9ebbdb743893a65a544405dd98bb503da88d37ba
paulnyabaro <paulnyabaro1@gmail.com>
2023-02-03 05:43:25 +0300

    Working on post edit

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', BLogListView.as_view(), name='home'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
     path('post/new/',BlogCreateView.as_view(), name='post_new'),
-    path('post/edit/<int:pk>/', BlogEditView.as_view(), name='post_edit'),
+    path('post/<int:pk>/edit/', BlogEditView.as_view(), name='post_edit'),
 ]
