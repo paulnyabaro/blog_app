1a4bbe30ef233990c1a641113f10528df59197d9
paulnyabaro <paulnyabaro1@gmail.com>
2023-02-03 04:49:07 +0300

    Added blog create view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 
@@ -11,5 +11,7 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
-class BlogCreateView():
-    pass
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
