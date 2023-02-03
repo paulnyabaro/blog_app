518c068fbd35b956aa820952073032e082af977e
paulnyabaro <paulnyabaro1@gmail.com>
2023-02-03 05:17:12 +0300

    Working on post edit

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BLogListView, BlogDetailView, BlogCreateView
+from .views import BLogListView, BlogDetailView, BlogCreateView, BlogEditView
 
 urlpatterns = [
     path('', BLogListView.as_view(), name='home'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
-    path('post/new/',BlogCreateView.as_view(), name='post_new')
+    path('post/new/',BlogCreateView.as_view(), name='post_new'),
+    path('post/edit/<int:pk>/', BlogEditView.as_view(), name='post_edit'),
 ]
