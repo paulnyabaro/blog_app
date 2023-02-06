363755a94fa597de10ab3c755e718777f01bf73d
paulnyabaro <paulnyabaro1@gmail.com>
2023-02-06 13:23:41 +0300

    Updated post delete view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
 
@@ -23,3 +24,5 @@ class BlogEditView(UpdateView):
 
 class BlogDeleteView(DeleteView):
     model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
